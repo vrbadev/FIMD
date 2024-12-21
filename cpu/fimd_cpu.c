f1a52f4f5ce3e96e277e50b294dcffa2e8fab206
Vojtěch Vrba <vrbavoj3@fel.cvut.cz>
2024-12-21 17:43:17 +0100

    Preprocessor macros prepended to the main library code

--- cpu/fimd_cpu.c
+++ cpu/fimd_cpu.c
@@ -3,13 +3,45 @@
 #include <string.h>
 
 #include "fimd_cpu.h"
-#include "cpp_magic.h"
 
+// Preprocessor macros to get function calls for each radius
+#define EVAL(...) EVAL1024(__VA_ARGS__)
+#define EVAL1024(...) EVAL512(EVAL512(__VA_ARGS__))
+#define EVAL512(...) EVAL256(EVAL256(__VA_ARGS__))
+#define EVAL256(...) EVAL128(EVAL128(__VA_ARGS__))
+#define EVAL128(...) EVAL64(EVAL64(__VA_ARGS__))
+#define EVAL64(...) EVAL32(EVAL32(__VA_ARGS__))
+#define EVAL32(...) EVAL16(EVAL16(__VA_ARGS__))
+#define EVAL16(...) EVAL8(EVAL8(__VA_ARGS__))
+#define EVAL8(...) EVAL4(EVAL4(__VA_ARGS__))
+#define EVAL4(...) EVAL2(EVAL2(__VA_ARGS__))
+#define EVAL2(...) EVAL1(EVAL1(__VA_ARGS__))
+#define EVAL1(...) __VA_ARGS__
+#define EMPTY()
+#define DEFER1(id) id EMPTY()
+#define DEFER2(id) id EMPTY EMPTY()()
+#define CAT(a, ...) a ## __VA_ARGS__
+#define FIRST(a, ...) a
+#define SECOND(a, b, ...) b
+#define IS_PROBE(...) SECOND(__VA_ARGS__, 0)
+#define PROBE() ~, 1
+#define NOT(x) IS_PROBE(CAT(_NOT_, x))
+#define _NOT_0 PROBE()
+#define BOOL(x) NOT(NOT(x))
+#define IF(c) _IF(BOOL(c))
+#define _IF(c) CAT(_IF_,c)
+#define _IF_0(...)
+#define _IF_1(...) __VA_ARGS__
+#define HAS_ARGS(...) BOOL(FIRST(_END_OF_ARGUMENTS_ __VA_ARGS__)(0))
+#define _END_OF_ARGUMENTS_(...) BOOL(FIRST(__VA_ARGS__))
+#define MAP(...) IF(HAS_ARGS(__VA_ARGS__))(EVAL(MAP_INNER(__VA_ARGS__)))
+#define MAP_INNER(op,sep,cur_val, ...) op(cur_val) IF(HAS_ARGS(__VA_ARGS__))(sep() DEFER2(_MAP_INNER)()(op, sep, ##__VA_ARGS__))
+#define _MAP_INNER() MAP_INNER
 
-#define FIMD_FN_TEMPLATE(_r_) uint8_t* fimd_r ## _r_ (uint8_t* img_ptr, uintptr_t* markers, uint32_t* markers_num, uintptr_t* sun_pts, uint32_t* sun_pts_num);
+#define FIMD_FN_TEMPLATE(_r_) extern uint8_t* fimd_r ## _r_ (uint8_t* img_ptr, uintptr_t* markers, uint32_t* markers_num, uintptr_t* sun_pts, uint32_t* sun_pts_num);
 #define FIMD_SWITCH_TEMPLATE(_r_) case _r_: fimd_r ## _r_ (tmp, markers_ptrs, markers_num, sun_pts_ptrs, sun_pts_num); break;
 
-MAP(FIMD_FN_TEMPLATE, EMPTY, FIMD_RADII)
+MAP(FIMD_FN_TEMPLATE, EMPTY, FIMD_RADII);
 
 const uint32_t fimd_radii_list[FIMD_RADII_COUNT] = { FIMD_RADII };
 
